b227df2fb774d628f948f1f897bb51a670a5576a
leonardo_Wer <name89213126414@gmail.com>
2024-11-14 13:31:48 +0300

    Решил первую задачу

--- src/lab4/task1.py
+++ src/lab4/task1.py
@@ -1,7 +1,7 @@
 """Алгоритм рекомендации просмотра фильмов"""
 
 
-def numbers_to_films(films_list:list, numbers_list:list) -> list:
+def numbers_to_films(films_list: list, numbers_list: list) -> list:
     """
     Сопоставляет названия фильмов и их номера
     >>> numbers_to_films([["1", "Хатико"], ["2", "Мстители"]], ["2", "2"])
@@ -54,39 +54,48 @@ class Recommendation:
         self.other_users_history = films_history()
         self.user_history = numbers_to_films(self.films_list, list(str(user_history)))
 
-    def get_user_history(self):
-        print(self.user_history)
+    def get_history(self):
+        """ Выводит историю пользователя и историю всех пользователей"""
+        print(f"История текущего пользователя: {self.user_history}")
+        print(f"История всех пользователей: {self.other_users_history}")
 
     def cnt_views(self, correct_films_list: list):
-        cnt_views_accepted_films = []
-        cnt_views = 0
+        """
+        Подсчитывает количество просмотров фильмов среди всех пользователей
+         - Принимает: список с фильмами
+         - Возвращает: список с просмотрами этих фильмов
+        """
+        cnt_views_accepted_films = [] # Список сот списками в которых: [количество просмотров, фильм]
         for film in correct_films_list:
+            cnt_views = 0
             for other_user_films in self.other_users_history:
                 for other_film in other_user_films:
                     if film == other_film:
                         cnt_views += 1
-            cnt_views_accepted_films.append(cnt_views)
+            cnt_views_accepted_films.append([cnt_views, film])
 
         return cnt_views_accepted_films
 
-    def film_recommendation(self):
-        accepted_films = []
+    def select_film_recommendation(self):
+        """ Подбирает рекомендацию для пользователя """
+        accepted_films = [] # Список рекомендаций для пользователя
         for film in self.user_history:
             for other_user_history in self.other_users_history:
                 cnt_correct_films = 0
-                for film_name in set(other_user_history):
+                for film_name in other_user_history:
                     if film_name == film:
                         cnt_correct_films += 1
-
                 if cnt_correct_films >= len(self.user_history):
-                    accepted_films.append(set(other_user_history + self.user_history) )
+                    accepted_films += ([film for film in other_user_history if film not in self.user_history])
 
         cnt_views_correct_films = self.cnt_views(accepted_films)
-        return max(cnt_views_correct_films)
+        user_recommendation = max(cnt_views_correct_films)[-1]
+        return user_recommendation
+
+    def get_user_recommendation(self):
+        print(f"Мы рекомендуем вам посмотреть фильм {self.select_film_recommendation()}!")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     person_recommendation = Recommendation(2, 4)
-    person_recommendation.get_user_history()
-    person_recommendation.film_recommendation()
-
+    person_recommendation.get_user_recommendation()
